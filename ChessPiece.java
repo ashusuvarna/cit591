10fb46e8e922d78399952c9172c3217f7e123a58
asuvarna <asuvarna@seas.upenn.edu>
2019-06-23 16:39:56 -0500

    This is the default Chess Piece Class created with some default comments

---
+++ ChessPiece.java
@@ -0,0 +1,21 @@
+/**
+ * This is a default ChessPiece class created for MCIT CIT-591 course
+ * at University of Pennsyalvania
+ */
+
+/**
+ * @author Ashu Suvarna
+ *
+ */
+public class ChessPiece {
+
+	String color;
+	String type;
+	
+	public ChessPiece(String pColor, String pType) {
+		
+		color = pColor;
+		type = pType;
+	}
+	
+}
